c981f0476e84f7201b6113e882f84404b1d4c221
Martin <MartinT@pressreader.com>
2019-09-27 10:58:25 -0700

    Seperated behaviour of board to another file

--- Assets/Scripts/Manager/BoardManager.cs
+++ Assets/Scripts/Manager/BoardManager.cs
@@ -78,34 +78,4 @@ namespace MPHT
             return mat.GetPlayerMaterialFor(MPHT.Utilities.RandomPlayer(players));
         }
     }
-
-    /// <summary>
-    /// Board Manager logic
-    /// </summary>
-    public class BoardManagerBehaviour
-    {
-        /// <summary>
-        /// Checks if the template board brick amount matches the amount of bricks on the scene
-        /// </summary>
-        /// <param name="boardTemplate">template for board</param>
-        /// <param name="board">board on the scene</param>
-        public void CheckTemplate(bool[] boardTemplate, GameObject[] board)
-        {
-            if (boardTemplate.Length != board.Length)
-            {
-                throw new System.Exception($"Board template amount not exact: board has {board.Length} bricks, template has {boardTemplate.Length} bricks");
-            }
-        }
-
-        /// <summary>
-        /// Returns the bricks which would make an outline of the shape
-        /// </summary>
-        /// <param name="boardTemplate">template of the board</param>
-        /// <returns>outlined shape of the board</returns>
-        public bool[] BoardOutlinedBricks(bool[] boardTemplate, int width)
-        {
-            // TODO
-            return boardTemplate;
-        }
-    }
 }
---
+++ Assets/Scripts/Manager/BoardManagerBehaviour.cs
@@ -0,0 +1,41 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BoardManagerBehaviour.cs" company="Martin Pak Hei Tsang">
+//     Copyright (c) Martin Pak Hei Tsang. 2019 All Rights Reserved
+// </copyright>
+//-----------------------------------------------------------------------
+namespace MPHT
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// Board Manager logic
+    /// </summary>
+    public class BoardManagerBehaviour
+    {
+        /// <summary>
+        /// Checks if the template board brick amount matches the amount of bricks on the scene
+        /// </summary>
+        /// <param name="boardTemplate">template for board</param>
+        /// <param name="board">board on the scene</param>
+        public void CheckTemplate(bool[] boardTemplate, GameObject[] board)
+        {
+            if (boardTemplate.Length != board.Length)
+            {
+                throw new System.Exception($"Board template amount not exact: board has {board.Length} bricks, template has {boardTemplate.Length} bricks");
+            }
+        }
+
+        /// <summary>
+        /// Returns the bricks which would make an outline of the shape
+        /// </summary>
+        /// <param name="boardTemplate">template of the board</param>
+        /// <returns>outlined shape of the board</returns>
+        public bool[] BoardOutlinedBricks(bool[] boardTemplate, int width)
+        {
+            // TODO
+            return boardTemplate;
+        }
+    }
+}
